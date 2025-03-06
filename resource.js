a688d3194efce827f73e6980a470f26abb51c1e4
AlekseyAleksandrov <axcerz@gmail.com>
2025-03-06 23:40:24 +0300

    prettier

--- resource.js
+++ resource.js
@@ -1,11 +1,10 @@
 class Resource {
-    constructor (name, quantity, consumptionRate, storageLocation){
-        this.name = name
-        this.quantity = quantity
-        this.consumptionRate = consumptionRate
-        this.storageLocation = storageLocation
-
-    }
+  constructor(name, quantity, consumptionRate, storageLocation) {
+    this.name = name;
+    this.quantity = quantity;
+    this.consumptionRate = consumptionRate;
+    this.storageLocation = storageLocation;
+  }
 }
 
-module.exports = {Resource}
+module.exports = { Resource };
