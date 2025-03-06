528b31d74c0a196efc1cd246e1f4305de51ad6b8
AlekseyAleksandrov <axcerz@gmail.com>
2025-03-07 00:00:31 +0300

    create: Task methods

--- task.js
+++ task.js
@@ -1,11 +1,28 @@
 class Task {
-    constructor (name, description, assignedCrewMember = null, requiredResources = [], requiredEquipment = []){
-        this.name = name
-        this.description = description
-        this.assignedCrewMember = assignedCrewMember 
-        this.requiredResources = requiredResources
-        this.requiredEquipment = requiredEquipment
-    }   
+  constructor(
+    name,
+    description,
+    assignedCrewMember = null,
+    requiredResources = [],
+    requiredEquipment = []
+  ) {
+    this.name = name;
+    this.description = description;
+    this.assignedCrewMember = assignedCrewMember;
+    this.requiredResources = requiredResources;
+    this.requiredEquipment = requiredEquipment;
+  }
+
+  assignCrewMember(crewMember) {
+    this.assignedCrewMember = crewMember;
+  }
+  addRequiredResource(resource) {
+    this.requiredResources.push(resource);
+  }
+  addRequiredAction() {}
+  addRequiredEquipment(equipment) {
+    this.requiredEquipment.push(equipment);
+  }
 }
 
-module.exports = {Task}
+module.exports = { Task };
