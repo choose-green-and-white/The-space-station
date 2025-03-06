fd4966200b4f8efd6906607ce1011007e1be54e3
AlekseyAleksandrov <axcerz@gmail.com>
2025-03-07 00:08:41 +0300

    create: class SpaceStation

--- spaceStation.js
+++ spaceStation.js
@@ -1,1 +1,24 @@
-// твой код здесь
+class SpaceStation {
+  constructor(crewMembers = [], resources = [], equipment = [], tasks = []) {
+    this.crewMembers = crewMembers;
+    this.resources = resources;
+    this.equipment = equipment;
+    this.tasks = tasks;
+  }
+
+  addCrewMembers(crewMember) {
+    this.crewMembers.push(crewMember);
+  }
+
+  addResources(resource) {
+    this.resources.push(resource);
+  }
+  addEquipment(equipment) {
+    this.equipment.push(equipment);
+  }
+  addTasks(task) {
+    this.tasks.push(task);
+  }
+}
+
+module.exports = { SpaceStation };
